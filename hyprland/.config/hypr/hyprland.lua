25498587dde5e4ddb58c6a7281d83d90f97e2057
Eraybeylik <eraybeylik3@gmail.com>
2026-09-01 15:25:52 +0300

    feat: force zero scaling for XWayland to avoid blurry X11 apps

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -216,6 +216,10 @@ hl.config({
     cursor = {
         no_hardware_cursors = true,
     },
+
+    xwayland = {
+        force_zero_scaling = true, -- no blurry XWayland apps
+    },
 })
 
 ---------------
